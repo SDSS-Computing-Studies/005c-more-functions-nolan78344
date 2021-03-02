c11106be0d8eeec382f474313e21630355cc43e6
nolano78344 <55156573+nolan78344@users.noreply.github.com>
2021-03-02 10:00:39 -0800

    sergfe

--- problem1.py
+++ problem1.py
@@ -12,11 +12,13 @@ Sample assertions:
 assert convertTemp(10,'C') == 50
 assert converTemp(32,'F') == 0
 """
-def celsius(a):
-    celsius = (a * 1.8) + 32
-    return a
+def convertTemp(temp, unit):
+    newunit = 0
+    if unit == "C":
+        newunit = (temp * 1.8) + 32
+    else:
+        newunit = (temp - 32) * (5/9)
+    return newunit
 
-
-def fahrenheit(x):
-    fahrenheit = (x - 32) * (5/9)
-    return x
+assert convertTemp(10,'C') == 50
+assert convertTemp(32,'F') == 0
